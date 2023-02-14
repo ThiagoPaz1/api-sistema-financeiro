67d26b2f9517e923b607398d06e12a535c2ffd0a
Thiago da Paz <thiagopazjunior@gmail.com>
2023-02-14 18:27:19 -0300

    remove

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 
 import { AppDataSource } from "./data-source"
-import { router } from "./routers"
 
 const app = express()
 
@@ -12,6 +11,5 @@ AppDataSource.initialize()
 
 app.use(cors())
 app.use(express.json())
-app.use("/", router)
 
 app.listen(process.env.PORT)
