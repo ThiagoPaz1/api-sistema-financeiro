c0ae17158b0282ba05adb5d73ad794d5a1b06250
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-02-16 21:50:49 -0300

    chore(ambient): added ambient to Vitor

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,15 @@ import "reflect-metadata"
 import "dotenv/config"
 
 import { DataSource } from "typeorm"
-import { dataSource } from './config/dataSource';
 
-export const AppDataSource = new DataSource(dataSource)
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/**/database/entities/*.{ts,js}`],
+	migrations: [`${__dirname}/**/database/migrations/*.{ts,js}`],
+    subscribers: [],
+})
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
 
-import { userController } from "../modules/user/controllers"
-import { validations } from "../modules/user/controllers/middlewares"
+// import { userController } from "../modules/user/controllers"
+// import { validations } from "../modules/user/controllers/middlewares"
 
 const userRouter = Router()
 
-userRouter.post("/", validations.validationFields, userController.newUser)
+// userRouter.post("/", validations.validationFields, userController.newUser)
 
 export { userRouter }
