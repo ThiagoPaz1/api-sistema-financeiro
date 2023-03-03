499d56943ac349dd0e226c68bc60c00f8d2b5a77
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-03-02 22:01:40 -0300

    fix(relations): add right columns

--- src/database/entities/transactions.ts
+++ src/database/entities/transactions.ts
@@ -7,7 +7,17 @@ export class Transactions {
   id?: number;
 
   @Column()
-  url?: string;
+  value!: string;
+
+  @Column()
+  category!: string;
+
+  // TODO implementar o array de Pagamentos Débito e Crédito para o type
+  @Column()
+  type!: string; // postgres não está aceitando arrays, depois vou verificar o motivo
+
+  @Column()
+  created_at?: Date;
 
   @ManyToOne(() => User, (user) => user.transactions)
   userId?: User;
