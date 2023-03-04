509e7056444a1325dbb41eacb5afe7e2882f30c4
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-03-03 21:41:25 -0300

    feat(token): boilerplate token

--- src/login/LoginController.ts
+++ src/login/LoginController.ts
@@ -1,32 +1,79 @@
-import { Request, Response } from 'express';
-import { sign } from 'jsonwebtoken';
-import { userRepository } from '../user/repository/userRepository';
+// import { Request, Response } from 'express';
+// import { sign } from 'jsonwebtoken';
 
-class TokenController {
-  async store(req: Request, res: Response) {
-    const { email = '', password = '' } = req.body;
+// import { User } from '../database/entities/user';
+// import { userRepository } from '../user/repository/userRepository';
 
-    if (!email || !password) {
-      return res.status(401).json({
-        errors: ['Credenciais Inválidas!'],
-      });
-    }
-    const user = await userRepository.findOneBy({ email });
-    if (!user) {
-      return res.status(401).json({
-        errors: ['Usuário não existe!'],
-      });
-    }
+// class AuthController {
+//   static login = async (req: Request, res: Response) => {
+//     //Check if username and password are set
+//     const { email, password } = req.body;
+//     if (!email && password) {
+//       res.status(400).send();
+//     }
 
-    const secretKey = process.env.TOKEN_SECRET as string;
+//     try {
+//       const user = await userRepository.findOneOrFail({ where: { email } });
 
-    const { id } = user;
-    const token = sign({ id, email }, secretKey, {
-      expiresIn: process.env.TOKEN_EXPIRATION,
-    });
+//       if (!user) {
+//         res.status(401).send();
+//         return;
+//       }
 
-    return res.json({ token });
-  }
-}
+//       //Sing JWT, valid for 1 hour
+//       const token = sign(
+//         { userId: user.id, username: user.username },
+//         config.jwtSecret,
+//         { expiresIn: '1h' },
+//       );
 
-export default new TokenController();
+//       //Send the jwt in the response
+//       res.send(token);
+//     } catch (error) {
+//       res.status(401).send();
+//     }
+
+
+
+//   };
+
+//   static changePassword = async (req: Request, res: Response) => {
+//     //Get ID from JWT
+//     const id = res.locals.jwtPayload.userId;
+
+//     //Get parameters from the body
+//     const { oldPassword, newPassword } = req.body;
+//     if (!(oldPassword && newPassword)) {
+//       res.status(400).send();
+//     }
+
+//     let user: User;
+//     try {
+//       user = await userRepository.findOneOrFail(id);
+
+//       //Check if old password matchs
+//       if (!user) {
+//         res.status(401).send();
+//         return;
+//       }
+  
+//       //Validate de model (password lenght)
+//       user.password = newPassword;
+//       const errors = await validate(user);
+//       if (errors.length > 0) {
+//         res.status(400).send(errors);
+//         return;
+//       }
+//       //Hash the new password and save
+//       user.hashPassword();
+//       userRepository.save(user);
+  
+//       res.status(204).send();
+
+//     } catch (id) {
+//       res.status(401).send();
+//     }
+
+//   };
+// }
+// export default AuthController;
