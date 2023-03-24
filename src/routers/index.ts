7df7583532c913fc32d88dd1abc535ce02e940f2
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-03-24 20:09:35 -0300

    feat(transaction): Routes OK

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { loginRouter } from './loginRouter';
+import { transactionRouter } from './transactionRouter';
 import { userRouter } from './userRouter';
 
 const router = Router();
 
 router.use(userRouter);
 router.use(loginRouter);
+router.use(transactionRouter)
 
 export { router };
