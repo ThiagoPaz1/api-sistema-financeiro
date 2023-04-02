5600e7091fe39028e6e8edfd40056f64e90e5716
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-04-02 15:39:06 -0300

    feat(validações): senha voltou a ser criptografada. Alterada validação para senha

--- src/database/entities/user.ts
+++ src/database/entities/user.ts
@@ -1,7 +1,16 @@
 import { Transactions } from './transactions';
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { IsEmail, IsNumber, IsOptional, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
+const regEx: RegExp = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])^.{6,32}$/;
 @Entity('tb_user')
 export class User {
   @PrimaryGeneratedColumn()
@@ -9,8 +18,8 @@ export class User {
 
   @Column()
   @IsString()
-  @MinLength(5)
-  @MaxLength(100)
+  @MinLength(5, { message: 'Nome tem que ter mais de 5 caracteres' })
+  @MaxLength(40, { message: 'Nome tem que conter menos que 40 caracteres' })
   name!: string;
 
   @Column({ unique: true })
@@ -19,22 +28,15 @@ export class User {
 
   @Column()
   @IsString()
-  @IsStrongPassword(
-    {
-      minLength: 6,
-      minUppercase: 1,
-      minSymbols: 1,
-      minNumbers: 1
-    },
-    {
-      message:
-      'Senha tem que conter mais que 6 caracteres, pelo menos uma letra maiúscula, pelo menos um caractere especial e pelo menos um número'
-    }
-  )
+  @Matches(regEx, {
+    message:
+      'Senha Fraca, tem que haver pelo menos um caractere especial, um número e uma letra maiúscula',
+  })
   password!: string;
 
   @Column({ nullable: true, default: 0 })
   @IsOptional()
+  @IsNumber()
   balance?: number;
 
   @OneToMany(() => Transactions, (transaction) => transaction.userId)
--- src/user/controllers/userController.ts
+++ src/user/controllers/userController.ts
@@ -1,125 +1,133 @@
-import { validate } from "class-validator";
+import { hashSync } from 'bcryptjs';
+import { validate } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
 import { userRepository } from '../repository/userRepository';
 
 class UserController {
-    async index(req: Request, res: Response) {
-      try {
-        const findAll = await userRepository.find({
-          relations: {
-            transactions: true
-          }
-        });
-        return res.status(200).json(findAll)
-      } catch (e: any | unknown) {
-        return console.error(e.message)
+  async index(req: Request, res: Response) {
+    try {
+      const findAll = await userRepository.find({
+        relations: {
+          transactions: true,
+        },
+      });
+      return res.status(200).json(findAll);
+    } catch (e: any | unknown) {
+      return console.error(e.message);
+    }
+  }
+
+  async userById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const findUser = await userRepository.findOne({
+        where: { id },
+        relations: {
+          transactions: true,
+        },
+      });
+
+      return res.status(200).json(findUser);
+    } catch (e: any | unknown) {
+      return console.error(e.message);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, email, password } = req.body;
+
+      const passwordHash = hashSync(password, 12);
+
+      const user = userRepository.create({
+        name,
+        email,
+        password: passwordHash,
+      });
+      const errors = await validate(user);
+
+      if (errors.length > 0) return res.status(404).json(errors);
+
+      await userRepository.save(user);
+      res.status(201).json(user);
+      return next();
+    } catch (e: any | unknown) {
+      return res.status(404).json({
+        message: {
+          email: `Email já pertence a um usuário`,
+          error: e.detail,
+        },
+      });
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const user = await userRepository.find({
+        where: {
+          email,
+          password,
+        },
+        relations: {
+          transactions: true,
+        },
+      });
+
+      if (!user) {
+        return res.status(401).json({ message: 'Usuario não existe' });
       }
+      return res.status(200).json('login efetuado');
+    } catch (e: any | unknown) {
+      console.error(e.message);
     }
-    
-    async userById(req: Request, res: Response) {
-        try {
-            const { id } = req.params;
-            const findUser = await userRepository.findOne({
-               where: { id },
-               relations: {
-                transactions: true,
-               } 
-              });
+  }
 
-            return res.status(200).json(findUser);
-        } catch (e: any | unknown) {
-            return console.error(e.message);
-        }
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { name, email, password, balance } = req.body;
+      const userUpdate = await userRepository.update(
+        {
+          id,
+        },
+        {
+          name,
+          email,
+          password,
+          balance,
+        },
+      );
+
+      const errors = await validate(userUpdate);
+
+      if (errors.length > 0) return res.status(404).json(errors);
+
+      return res.status(201).json(userUpdate);
+    } catch (e: any | unknown) {
+      console.error(e.message);
     }
+  }
 
-    async create(req: Request, res: Response, next: NextFunction) {
-        try {
-          const { name, email, password, } = req.body;
-
-          const user = userRepository.create({
-            name,
-            email,
-            password
-          });
-          const errors = await validate(user);
-          
-          if (errors.length > 0)
-            return res.status(404).json(errors);
-          await userRepository.save(user);
-          res.status(201).json(user);
-          return next();
-        } catch (e: any | unknown) {
-          return res.status(404).json({ message: {
-            email: `Email já pertence a um usuário`,
-            error: e.detail
-          } });
-        } 
+  async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      await userRepository.delete(id);
+      return res.status(204).json('usuario deletado com sucesso');
+    } catch (e: any | unknown) {
+      console.log(e.message);
     }
+  }
 
-    async login(req: Request, res: Response){
-        try {
-          const {email, password} = req.body;
-          const user = await userRepository.find({
-            where: {
-            email,
-            password
-          },
-          relations: {
-            transactions: true,
-          }
-        });
-
-          if(!user){
-            return res.status(401).json({message:"Usuario não existe"})
-          }
-          return res.status(200).json("login efetuado")
-          
-        } catch (e: any | unknown) {
-          console.error(e.message)
-        }
-      }
-
-    async update(req: Request, res: Response) {
-      try {
-          const { id } = req.params;
-          const { name, email, password, balance } = req.body;
-          const userUpdate = await userRepository.update({
-              id
-          },
-          {
-              name,
-              email,
-              password,
-              balance
-          })
-        
-         return res.status(201).json(userUpdate)
-      } catch (e: any | unknown) {
-          console.error(e.message)
-      }
-    
+  async deleteAllUsers(req: Request, res: Response) {
+    try {
+      const user = (await userRepository.find()) as unknown as string;
+      await userRepository.delete(user);
+      return res.status(204).json('usuario deletado com sucesso');
+    } catch (e: any | unknown) {
+      console.log(e.message);
     }
-
-    async delete(req: Request, res: Response) {
-      try {
-        const { id } = req.params
-        await userRepository.delete(id);
-        return res.status(204).json('usuario deletado com sucesso');
-      } catch (e: any | unknown) {
-        console.log(e.message);
-      }
-    }
-
-    async deleteAllUsers(req: Request, res: Response) {
-        try {
-          const user = await userRepository.find() as unknown as string;
-          await userRepository.delete(user);
-          return res.status(204).json("usuario deletado com sucesso");
-      } catch (e: any | unknown) {
-          console.log(e.message)
-        }
-    }
-
-};
+  }
+}
 
 export default new UserController();
